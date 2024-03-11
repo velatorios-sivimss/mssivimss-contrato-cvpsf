8314cb13f5fbc4b6c912df74f158f494ee2ed948
LeandroPeopleMedia <107154425+LeandroPeopleMedia@users.noreply.github.com>
2024-03-11 11:35:09 -0600

    consulta paginada

--- src/main/java/com/imss/sivimss/contratocvpps/service/NuevoPlanSFPAService.java
+++ src/main/java/com/imss/sivimss/contratocvpps/service/NuevoPlanSFPAService.java
@@ -2,12 +2,16 @@ package com.imss.sivimss.contratocvpps.service;
 
 import java.io.IOException;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import com.imss.sivimss.contratocvpps.util.Response;
 
 public interface NuevoPlanSFPAService {
 
 	public Response<Object> detallePlanSFPA(Integer idPlanSFPA, Authentication authentication)
+			throws IOException;
+
+	public ResponseEntity<Object> busquedaPlanSFPA(Integer idPlanSFPA, Authentication authentication)
 			throws IOException;
 
 }
